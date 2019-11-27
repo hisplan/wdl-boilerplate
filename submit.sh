cf7d3a5aeb2542edc3d9e84718152db82a77b4d8
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2019-11-26 20:14:32 -0500

    Add -i

--- submit.sh
+++ submit.sh
@@ -5,15 +5,17 @@ usage()
 cat << EOF
 USAGE: `basename $0` [options]
     -k  service account key (e.g. secrets.json)
+    -i  inputs file (e.g. sample.inputs.json)
     -l  labels file (e.g. labels.json)
     -o  options file (e.g. options.json)
 EOF
 }
 
-while getopts "k:l:o:h" OPTION
+while getopts "k:i:l:o:h" OPTION
 do
     case $OPTION in
         k) service_account_key=$OPTARG ;;
+        i) inputs_file=$OPTARG ;;
         l) labels_file=$OPTARG ;;
         o) options_file=$OPTARG ;;
         h) usage; exit 1 ;;
@@ -21,7 +23,7 @@ do
     esac
 done
 
-if [ -z "$service_account_key" ] || [ -z "$labels_file" ] || [ -z "$options_file" ]
+if [ -z "$service_account_key" ] || [ -z "$inputs_file" ] || [ -z "$labels_file" ] || [ -z "$options_file" ]
 then
     usage
     exit 1
@@ -33,7 +35,7 @@ zip Main.deps.zip modules modules/*
 cromwell-tools submit \
     --secrets-file ${service_account_key} \
     --wdl Main.wdl \
-    --inputs-files Main.inputs.json \
+    --inputs-files ${inputs_file} \
     --deps-file Main.deps.zip \
     --label-file ${labels_file} \
     --options-file ${options_file}
